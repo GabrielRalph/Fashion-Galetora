16da325233a8b4b64d658673bd404617a598759b
GabrielRalph <gltraph@gmail.com>
2020-02-10 02:20:10 +1100

    Firebase Connection, Search, Load screen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <works/>
+  <div>
+    <div v-if = "!hide" :class = "{'load-screen':true, fade: loaded}">
+      <div class = "center">
+          <h1>GABRIEL</h1>
+          <h1>LEONARD</h1>
+          <h1>Φ</h1>
+      </div>
+    </div>
+    <works v-if = "loaded" :database = "database"/>
+  </div>
 </template>
 
 <script>
 import works from './components/works';
+import firebase from 'firebase'
+// Your web app's Firebase configuration
+ var firebaseConfig = {
+   apiKey: "AIzaSyDniS0prRjEmOyKbMd4jequo9gkwe2otKI",
+   authDomain: "fashion-galetora.firebaseapp.com",
+   databaseURL: "https://fashion-galetora.firebaseio.com",
+   projectId: "fashion-galetora",
+   storageBucket: "fashion-galetora.appspot.com",
+   messagingSenderId: "469438762797",
+   appId: "1:469438762797:web:759f543ce82183b9f04da4",
+   measurementId: "G-Q7DJ37H3D0"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
 
 export default {
   name: 'App',
@@ -13,24 +36,69 @@ export default {
   },
 
   data: () => ({
-    //
+    loaded: false,
+    hide: false,
+    database: {}
   }),
+  created(){
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--show', `${window.innerHeight}px`);
+
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+    firebase.database().ref('test-database').once('value').then((sc) => {
+      this.database = sc.val();
+      this.loaded = true;
+      setTimeout(() => {this.hide = true}, 500)
+    })
+  }
 };
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono:500&display=swap');
 body{
   margin: 0;
   padding: 0;
+  font-family: 'Roboto Mono', monospace;
+
 }
 .container{
   padding: 0;
   /* height: 100%; */
 }
-.v-window__conatiner{
-  /* height: 100%; */
+.load-screen{
+  width: 100%;
+  height: 100%;
+  padding: 50px;
+  position: fixed;
+  z-index: 1000;
+  background: rgba(255, 253, 245);
+  transition: 0.5s ease-in;
 }
-.v-window{
-  /* height: 100%; */
+.load-screen h1{
+  text-align: center;
+  line-height: 50px;
 }
+.load-screen:not(.fade){
+  opacity: 1;
+}
+.fade{
+  opacity: 0;
+}
+.center{
+  position: fixed;
+  height: 150px;
+  margin: auto;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
 </style>
--- src/components/works.vue
+++ src/components/works.vue
@@ -1,21 +1,35 @@
 <template>
     <v-window active-class = "windows" v-model = "place" :reverse = "reverse" :touch = "{left, right}">
-      <v-window-item v-for = "(item, i) in database" :key = "i">
+      <tr :class = "{'search-bar': true, 'show-search': showSearch}">
+        <td style = "width: 100%">
+          <input type = "text" class = "search" pattern="[A-Z]" v-model= "filter" placeholder="SEARCH KEYWORDS"/>
+        </td>
+        <td style = "padding-right: 10px">
+          <v-icon color = "#fff" v-if = "filter.length > 0" @click = "filter = ''; showSearch = false">close</v-icon>
+        </td>
+        <td style = "padding-right: 10px">
+          <v-icon color = "#fff" @click = "showSearch = false">search</v-icon>
+        </td>
+      </tr>
+      <h1 style = "padding: 50px 20px" v-if = "databaseFiltered.length == 0">THERE'S NOTHING HERE FOR YOU</h1>
+      <v-window-item v-for = "(item, i) in databaseFiltered" :key = "i + 'd'">
         <v-img eager :src = "item.disp_image_url" min-height = "calc(var(--vh, 1vh) * 100)"></v-img>
-        <div class = "more-info" @click = "showInfo">
-            <h2>^</h2>
+        <div :class = "{'more-info':true, scroll:show}" @click = "showInfo">
+          <div style = "text-align: center">
+
+            <v-icon class = "expand-icon">expand_less</v-icon>
+          </div>
             <h1>{{item.title.toUpperCase()}}</h1>
             <h3>IN {{item.color.toUpperCase()}} {{item.material.toUpperCase()}}</h3>
             <div class = "break"></div>
-            <template v-for = "(detail, i) in item.details">
-              <v-img :src = "detail.image_url" :key = "i">
+            <div v-for = "(detail, j) in item.details" :key = "j + 'a'" class = "detail-card">
+              <v-img :src = "detail.image_url" :key = "j + 'b'">
               </v-img>
-              <h4>{{detail.caption}}</h4>
-              <h4>{{detail.description}}</h4>
-            </template>
-            <div class = "break"></div>
-            <div class = "break"></div>
-            <h2>Message me if you're interested</h2>
+              <div class = "detail-caption">
+                {{detail.caption}}
+              </div>
+              <div class = "detial-description" :key = "j + 'c'">{{detail.description}}</div>
+            </div>
             <div class = "break"></div>
         </div>
       </v-window-item>
@@ -24,30 +38,43 @@
 </template>
 
 <script>
-import mocbase from '../assets/mocbase.json'
   export default {
     name: 'HelloWorld',
 
+    props: {
+      database: {
+        type: Array,
+        required: true
+      }
+    },
     data: () => ({
-      database: mocbase.database,
       place: 1,
       reverse: false,
       height: 0,
       show:false,
+      scroll: 0,
+      filter: '',
+      showSearch: false,
+      moving: false
     }),
 
     methods: {
+
       left(){
         this.reverse = false;
         this.place++;
         this.show = false;
         document.documentElement.style.setProperty('--show', `${window.innerHeight}px`);
+        this.moving = true
+        setTimeout(() => {this.moving = false}, 500)
       },
       right(){
         this.show = false;
         document.documentElement.style.setProperty('--show', `${window.innerHeight}px`);
         this.reverse = this.place == 0
         this.place = (this.place - 1 + this.database.length)%this.database.length
+        this.moving = true
+        setTimeout(() => {this.moving = false}, 500)
       },
       showInfo(){
         if(this.show){
@@ -63,33 +90,46 @@ import mocbase from '../assets/mocbase.json'
     computed:{
       screenHeigth(){
         return this.height + 'px'
+      },
+      databaseFiltered(){
+        if(this.filter.length > 1){
+          var database_filtered = []
+          for(var i in this.database){
+            if(this.database[i].keywords.toUpperCase().indexOf(this.filter.toUpperCase())!= -1){
+              database_filtered.push(this.database[i]);
+            }
+          }
+          return database_filtered
+        }else{
+          return this.database
+        }
       }
     },
     created(){
       let scrollY = 0;
-      let moving = false;
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-      document.documentElement.style.setProperty('--show', `${window.innerHeight}px`);
 
-      window.addEventListener('resize', () => {
-        // We execute the same script as before
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-      });
       window.addEventListener('scroll', () => {
         scrollY = window.scrollY;
-        if(!moving){
-          if(scrollY > 1){
-            moving = true;
-            this.show = true;
-            document.documentElement.style.setProperty('--show', `0px`);
-            setTimeout(() => {moving = false}, 300)
-          }else if(scrollY < -1){
-            moving = true
-            this.show = false;
-            document.documentElement.style.setProperty('--show', `${window.innerHeight}px`);
-            setTimeout(() => {moving = false}, 300)
+        this.scroll = scrollY;
+        if(!this.moving){
+          if(scrollY > 20){
+            if(this.showSearch){
+              this.showSearch = false;
+            }else{
+              this.show = true;
+              document.documentElement.style.setProperty('--show', `0px`);
+            }
+            this.moving = true;
+            setTimeout(() => {this.moving = false}, 500)
+          }else if(scrollY < -20){
+            if(this.show){
+              this.show = false;
+              document.documentElement.style.setProperty('--show', `${window.innerHeight}px`);
+            }else{
+              this.showSearch = true;
+            }
+            this.moving = true
+            setTimeout(() => {this.moving = false}, 500)
           }
         }
       });
@@ -111,7 +151,6 @@ img{
   transition: 0.3s ease-in;
   width: 100%;
   height: calc(var(--vh, 1vh) * 100 + 30px);
-  overflow: scroll;
   webkit-backdrop-filter: blur(calc(25px - var(--show)));
   backdrop-filter: blur(calc(25px - var(--show)));
   padding: 0 20pt;
@@ -120,14 +159,49 @@ img{
 
 }
 .scroll{
-  overflow: hidden;
+  overflow: scroll;
 }
-.more-info h2{
+.expand-icon{
   text-align: center;
   line-height: 30px;
   padding-bottom: 20pt;
 }
 .break{
   height: 40px;
+}
+.detail-caption{
+  font-size: 16px;
+  font-style: italic;
+  text-align: center;
+  padding-bottom: 10px;
+}
+.detail-description{
+  font-size: 18px;
+}
+.detail-card{
+  padding-bottom: 30px;
+}
+.search{
+  font-size: 20px;
+  outline: none;
+  border: none;
+  padding: 10px;
+  color: white;
+}
+.show-search{
+  top: 0px;
+}
+.search-bar:not(.show-search){
+  top: -50px;
+}
+.search:focus{
+  outline: none;
+  border: none;
+}
+.search-bar{
+  transition: 0.3s ease-in;
+  position: fixed;
+  z-index: 100;
+  background: rgb(50, 50, 55);
 }
 </style>
